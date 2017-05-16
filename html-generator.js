80a3a32b9ea6d88f95cb14d1436bef08c32c47a9
Justin LeFurjah <lefurjah@gmail.com>
2017-05-16 13:54:00 -0500

    add script to generate html from readme files

---
+++ html-generator.js
@@ -0,0 +1,39 @@
+
+// This script is dependent on showdown.js https://github.com/showdownjs/showdown
+// npm install showdown
+// To run the script: node html-generator.js
+
+console.log("Beginning HTML generation script");
+var showdown  = require('showdown');
+var converter = new showdown.Converter();
+var fs = require('fs');
+
+
+// To add a new library, simply replicate the existing file structure
+// languagename/README.md
+// and add the name of the library to the array below
+
+libraries = [ 'php', 'python', 'ruby']
+
+function build_html(libraries){
+  libraries.forEach(function(element) {
+    var text = fs.readFileSync(`${element}/README.md`, 'utf-8');
+    var md = converter.makeHtml(text);
+    fs.writeFileSync(`${element}/index.html`, md, 'utf-8');
+    console.log(`HTML for ${element}/index.html generated successfully.`)
+  }); 
+}
+
+function build_main_html(){
+  var text = fs.readFileSync('README.md', 'utf-8');
+  var md = converter.makeHtml(text);
+  fs.writeFileSync(`index.html`, md, 'utf-8');
+  console.log('HTML for the main index.html generated successfully.')
+}
+
+function execute_script(){
+  build_html(libraries)
+  build_main_html();
+}
+
+execute_script();
