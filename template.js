81d09597677d35cfb24482e043e82da2010a5f3f
Justin LeFurjah <interestinall@users.noreply.github.com>
2017-05-25 11:26:32 -0500

    Html (#5)
    updated main index.html links, commented out the PHP todo
    updated how the main index.html is created

--- template.js
+++ template.js
@@ -1,11 +1,12 @@
-exports.header = function(language) {
+exports.header = function(title) {
+
   return `<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Vivial Connect ${language} Library</title>
+        <title>Vivial Connect ${title}</title>
         <link rel="stylesheet" href="/css/github-markdown.css">
         <link rel="stylesheet" href="/css/style.css">
         <link rel="shortcut icon" href="/img/favicon.png'">
